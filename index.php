38eb639a7e3a73cd3934506742eea8d27bd53adb
mica-ielle <96358236+mica-ielle@users.noreply.github.com>
2022-01-21 01:04:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<body>
+<center>
+	<h1>* * * * WELCOME * * * *</h1>
+	<h2>LOGIN AS</h2>
+</center>
+<div id="general">
+	<table align="center">
+		<tr>
+	<form action="login.php?" method="post" >
+			<td>
+			<center>
+				<input type="submit" name="entrer" value="ADMIN" />
+			</center>
+		   </td>
+		   
+	</form>
+		</tr>
+		<tr>
+	<form action="login.php?"  >
+			<td>
+			<center>
+				<input type="submit" name="entrer" value="TEACHER" />
+			</center>
+		   </td>
+	</form>
+		</tr>
+		<tr>
+	<form action="student_login.php?" >
+			<td>
+			<center>
+				<input type="submit" name="entrer" value="STUDENT" />
+			</center>
+		   </td>
+	</form>
+		</tr>
+	</table>
+</table>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+
+<body>
+<center>
+	<h1>ADMIN LOGIN</h1>
+
+<div id="general">
+	<table align="center">
+		<tr>
+	<form action="ac_admin.php?" method="POST" >
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email" class="form-control"/>
+		</div>
+		<div class="form-group">
+			<label for="password"></br>password</label>
+			<input type="password" id="password" name="password" class="form-control"/>
+		</div></br>
+        <div class="m-t-20 text-center">
+			<button class="btn btn-primary submit-btn" name="login">login</button>
+        </div></br>
+	</form>
+	<form method="post" action="index.php?">
+		<input type="submit" name="entrer" value="BACK" />
+	</form>
+		</tr>
+	</table>
+</table>
+</div>
+</center>
+</body>
+</html>
