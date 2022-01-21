5710a2e42f5ead9e76ba4528845d49a83061f433
mica-ielle <96358236+mica-ielle@users.noreply.github.com>
2022-01-21 03:33:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/style.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/bootstrap-datetimepicker.min.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/dataTables.bootstrap4.min.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/font-awesome.min.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/fullcalendar.min.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/select2.min.css"/>
+<link rel="stylesheet" type="text/css" href="/Projet(system_management)/assets/css/tagsinput.css"/>
 <body>
 <center>
 	<h1>* * * * WELCOME * * * *</h1>
-	<h2>LOGIN AS</h2>
+	<h2>LOGIN AS</br></br></br></br></br></h2>
 </center>
 <div id="general">
 	<table align="center">
@@ -10,17 +18,17 @@
 	<form action="login.php?" method="post" >
 			<td>
 			<center>
-				<input type="submit" name="entrer" value="ADMIN" />
+				<input type="submit" name="entrer" value="ADMIN" /></br></br>
 			</center>
 		   </td>
 		   
 	</form>
 		</tr>
 		<tr>
-	<form action="login.php?"  >
+	<form action="teacher_login.php?"  >
 			<td>
 			<center>
-				<input type="submit" name="entrer" value="TEACHER" />
+				<input type="submit" name="entrer" value="TEACHER" /></br></br>
 			</center>
 		   </td>
 	</form>
